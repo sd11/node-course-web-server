4b64bcc529be10231d09183666bedd3fdc052dd7
Shatabdi Dey <Shatabdi.Dey@mi9.com.au>
2017-06-23 11:18:30 +1000

    test commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next)=>{
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err)=>{
         if(err){
-            console.log('Unable to append to server.log.')
+            console.log('Unable to append to server.log anymore.')
         }
     });
     next();
